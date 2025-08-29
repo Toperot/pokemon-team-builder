d96a6694af49090af0111487fa80ca6afa0d5a57
Kristoffer Ludovice <kristoffer_noli_ludovice@dlsl.edu.ph>
2025-08-29 23:27:40 +0800

    colorlang

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import TeamView from "./components/TeamView";
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-blue-100 text-gray-900">
+    <div className="min-h-screen bg-blue-200 text-gray-900">
       <header className="max-w-6xl mx-auto p-6">
         <h1 className="text-3xl font-extrabold text-red-600 mb-4 text-outline">Dream Pokémon Team Builder</h1>
         <p className="mt-1 font-bold text-sm text-black-600 mb-4">Pick up to 6 from the first 151 Pokémon.</p>
